4420dcbbc5a32de79edfbc0e5e8f453f9db5a8c5
transposons-thesis <141131851+transposons-thesis@users.noreply.github.com>
2023-08-01 11:13:20 +0100

    Update file_uploader.sh

--- file_uploader.sh
+++ file_uploader.sh
@@ -1,30 +1,20 @@
 #file upload
-rsync -avz /Volumes/Expansion/20230512_rb_cello_l1egfp/20230512_rb_cello_l1egfp/20230512_1228_1C_PAK73482_bd039484/fastq_pass/ grte3662@gateway.arc.ox.ac.uk:/data/paed-transponsons-long-reads/grte3662/data
+# with rsync -avz command to cluster
 
-#!/bin/sh
-#SBATCH -o per.out
-#SBATCH -e per.err
-#SBATCH --partition=devel
-#SBATCH --qos=standard
-#SBATCH --job-name=per
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=10G
-#SBATCH --mail-user=francesco.rivetti@gtc.ox.ac.uk
-
+# how many reads?
 wc -l filtered_insertion_32.fastq
 
 # combine fastq, unzip and split for ease
 gunzip *fastq.gz
-cat *.fastq > insertion_32.fastq 
+cat *.fastq > insertion.fastq 
 
 # remove super long reads
-wc -l insertion_32.fastq
+wc -l insertion.fastq 
 awk 'BEGIN {FS = "\t" ; OFS = "\n"} {header = $0 ; getline seq ; getline qheader ; getline qseq ; if (length(seq) < 20000) {print header, seq, qheader, qseq}}' < insertion_32.fastq > processed/filtered_insertion_32.fastq
-wc -l processed/filtered_insertion_32.fastq
+wc -l processed/filtered_insertion.fastq
 
 # split massive file 
-split -d -l 40000000 filtered_insertion_32.fastq split/sub_fil_insertion_t
+split -d -l 40000000 filtered_insertion.fastq split/sub_fil_insertion_t
 
 # 10% file
 f
